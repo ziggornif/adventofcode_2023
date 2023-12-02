76677dbb9988e7d29fbb5a35e3db4462266d58d0
ziggornif <matthieudrouian@gmail.com>
2023-12-02 18:34:12 +0100

    feat: d2 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,9 +1,39 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 
+use regex::Regex;
+
 fn read_input_file(input: String) -> Result<BufReader<File>> {
     let file = File::open(input)?;
     Ok(BufReader::new(file))
+}
+
+struct Count {
+    color: String,
+    count: i32,
+}
+
+fn extract_game_number(input: &str) -> i32 {
+    let re = Regex::new(r"Game (\d+):").unwrap();
+    if let Some(captures) = re.captures(&input) {
+        if let Some(game_number) = captures.get(1) {
+            return game_number.as_str().parse().unwrap();
+        }
+    }
+    0 // Default value if not found
+}
+
+fn extract_counts(input: &str) -> Vec<Count> {
+    let mut counts = Vec::new();
+    let re = Regex::new(r"(\d+) (blue|red|green)").unwrap();
+
+    for captures in re.captures_iter(&input) {
+        let count = captures[1].parse().unwrap();
+        let color = captures[2].to_string();
+        counts.push(Count { color, count });
+    }
+
+    counts
 }
 
 fn sum_of_games(input: String, red: i32, green: i32, blue: i32) -> Result<i32> {
@@ -15,6 +45,27 @@ fn sum_of_games(input: String, red: i32, green: i32, blue: i32) -> Result<i32> {
         match line {
             Ok(line_content) => {
                 // here the magic
+                let game_number = extract_game_number(&line_content);
+                let counts = extract_counts(&line_content);
+
+                let red_arr = counts
+                    .iter()
+                    .filter(|item| item.color == "red")
+                    .find(|item| item.count > red);
+                let green_arr = counts
+                    .iter()
+                    .filter(|item| item.color == "green")
+                    .find(|item| item.count > green);
+                let blue_arr = counts
+                    .iter()
+                    .filter(|item| item.color == "blue")
+                    .find(|item| item.count > blue);
+
+                if red_arr.is_some() || green_arr.is_some() || blue_arr.is_some() {
+                    println!("Invalid game");
+                } else {
+                    sum += game_number;
+                }
             }
             Err(e) => eprintln!("Error reading line: {}", e),
         }
