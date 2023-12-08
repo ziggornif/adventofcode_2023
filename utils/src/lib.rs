7993adc791d8574f5b6a112a99286c5535cb4e56
Matthieu Drouian <matthieudrouian@gmail.com>
2023-12-08 08:15:29 +0100

    feat: improve utils

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,7 +1,13 @@
 use std::fs::File;
-use std::io::{BufReader, Result};
+use std::io::{BufRead, BufReader, Error};
 
-pub fn read_input_file(input: String) -> Result<BufReader<File>> {
+pub fn read_input_file(input: String) -> Result<BufReader<File>, Error> {
     let file = File::open(input)?;
     Ok(BufReader::new(file))
+}
+
+pub fn read_input_file_as_vec(input: String) -> Result<Vec<String>, Error> {
+    let reader = read_input_file(input)?;
+    let lines_arr: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
+    Ok(lines_arr)
 }
